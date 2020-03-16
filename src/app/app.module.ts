991864ec983937b7540bfa10537728c250a008d3
markflm <38789101+markflm@users.noreply.github.com>
2020-03-15 22:51:08 -0400

    created necessary components, added image assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PlayercardComponent } from './components/playercard/playercard.component';
+import { ResulttableComponent } from './components/resulttable/resulttable.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentComponent
+    HomeComponentComponent,
+    NavbarComponent,
+    PlayercardComponent,
+    ResulttableComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/resulttable/resulttable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resulttable',
+  templateUrl: './resulttable.component.html',
+  styleUrls: ['./resulttable.component.css']
+})
+export class ResulttableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
