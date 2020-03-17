c9088da39ff7bd6ba7ec95ac49f77470e22b3501
markflm <38789101+markflm@users.noreply.github.com>
2020-03-17 14:07:59 -0400

    added resulttable template, put in grid styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core'; //pulling in ViewEncapsulation to allow this component's css to affect app globally
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'pointcheck-front-end';
--- src/app/components/resulttable/resulttable.component.ts
+++ src/app/components/resulttable/resulttable.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResulttableComponent implements OnInit {
 
+  gameList = [
+    {date:"1/1/2011",
+    link:"link dot com",
+    map: "Narrows",
+    gametype: "Slayer",
+    playlist: "MLG"},
+    {date:"2/2/2009",
+    link:"link dot org",
+    map: "The Pit",
+    gametype: "Flag",
+    playlist: "MLG"}
+  ]
   constructor() { }
 
   ngOnInit(): void {
